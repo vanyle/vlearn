73d2e4b1c44dee00e73b74a26a4dbaf2ca7e04fa
vanyle <delegue.antoine@gmail.com>
2020-12-14 19:28:10 +0100

    Initial commit

---
+++ src/test/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
+// Mettre ici les fonctions qui testent la biliothèque.
+int main(int argc,char ** argv){
+	int b = addOne(5);
+
+	cout << b;
+}
